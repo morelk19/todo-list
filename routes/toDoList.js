fee2194ea7d7e54e454c0936b9d7f9afb54b8d67
morelk19 <morelkhristian@gmail.com>
2023-02-19 17:05:46 -0500

    added create-many route and createManyTasks function. Got it working for an array of objects being sent in.

--- routes/toDoList.js
+++ routes/toDoList.js
@@ -8,10 +8,10 @@ const tasksController = require('../controllers/TasksController');
 //CRUD OPERATIONS 
 router.get('/all', tasksController.getAllTasks);
 router.post("/create-one", tasksController.createOneTask);
+router.post("/create-many", tasksController.createManyTasks);
+router.delete("/delete-one/:id", tasksController.deleteOneTask);
+router.delete("/delete-many", tasksController.deleteManyTasks);
 router.get("/get-one/:id", tasksController.getOneTask);
 router.put("/update-one/:id", tasksController.updateOneTask);
-router.delete("/delete-one/:id", tasksController.deleteOneTask);
-//router.delete("/delete-many", tasksController.deleteManyTasks);
-router.delete("/delete-many", tasksController.deleteManyTasks);
 
 module.exports = router;
